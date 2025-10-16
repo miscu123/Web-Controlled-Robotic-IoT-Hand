65fd06a3d3905293e364933654bbe191dabc7238
miscu123 <70350848+miscu123@users.noreply.github.com>
2025-10-16 15:48:47 +0300

    Succesfully configured ESP32 board
    Board needs to be connected to COM7, but other than that is works just fine and implemented (flashed) first piece of code on it. (works properly)
    Next step would be to try and implement a finger algorithm to set it in specified posiitons.

--- Robotic-Hand/src/Backend-Server/main.cpp
+++ Robotic-Hand/src/Backend-Server/main.cpp
@@ -1,13 +1,32 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+
+Servo servo;
+const int servoPin = 18;
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("ESP32 este in functiune!");
+  servo.attach(servoPin);
+  Serial.println("Servo test automat");
 }
 
 void loop()
 {
-  Serial.println("Ruleaza loop...");
-  delay(1000);
+  for (int angle = 0; angle <= 180; angle += 3)
+  {
+    servo.write(angle);
+    Serial.print("Unghi: ");
+    Serial.println(angle);
+    delay(8);
+  }
+  delay(200);
+  for (int angle = 180; angle >= 0; angle -= 3)
+  {
+    servo.write(angle);
+    Serial.print("Unghi: ");
+    Serial.println(angle);
+    delay(8);
+  }
+  delay(500);
 }
